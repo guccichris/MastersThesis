7739ab9c7a2ad2ef47b8fac44adf6f08da17ca91
Christopher Parker <cjp95@zips.uakron.edu>
2017-09-29 13:56:46 -0400

    created empty file to use in debugging for loops

---
+++ assignment4/debug_gradFlow.py
@@ -0,0 +1,30 @@
+# File Name: debug_gradFlow.py
+# Description: Trying the looping in gradientFlow.py with a known function
+#               to ensure that I am getting the proper output
+# Author: Christopher Parker
+# Created: Fri Sep 29, 2017 | 01:54P EDT
+# Last Modified: Fri Sep 29, 2017 | 01:55P EDT
+
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
+#                           GNU GPL LICENSE                            #
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
+#                                                                      #
+# Copyright Christopher Parker 2017 <cjp65@case.edu>                   #
+#                                                                      #
+# This program is free software: you can redistribute it and/or modify #
+# it under the terms of the GNU General Public License as published by #
+# the Free Software Foundation, either version 3 of the License, or    #
+# (at your option) any later version.                                  #
+#                                                                      #
+# This program is distributed in the hope that it will be useful,      #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of       #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the         #
+# GNU General Public License for more details.                         #
+#                                                                      #
+# You should have received a copy of the GNU General Public License    #
+# along with this program. If not, see <http://www.gnu.org/licenses/>. #
+#                                                                      #
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
+
+
+
