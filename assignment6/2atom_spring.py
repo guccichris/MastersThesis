e67f0ae79c32a62b654f5228591baab6d3948412
Christopher Parker <cjp95@zips.uakron.edu>
2017-10-16 12:37:46 -0400

    Standard commit.

--- assignment6/2atom_spring.py
+++ assignment6/2atom_spring.py
@@ -1,8 +1,9 @@
-# File Name: debug.py
-# Description: Trying various plots for debugging gradientFlow.py
+# File Name: 2atom_spring.py
+# Description: This script will model two bonded atoms floating in space by
+#               connecting them with a spring
 # Author: Christopher Parker
-# Created: Thu Sep 28, 2017 | 12:05P EDT
-# Last Modified: Tue Oct 10, 2017 | 11:35P EDT
+# Created: Mon Oct 16, 2017 | 12:35P EDT
+# Last Modified: Mon Oct 16, 2017 | 12:36P EDT
 
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
 #                           GNU GPL LICENSE                            #
@@ -26,51 +27,7 @@
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
 
 import numpy as np
-from gradientFlow import vdwForce
-#import matplotlib as mpl
+import scipy as sci
+from IPython import embed
 
-#mpl.use('agg')
 
-import matplotlib.pyplot as plt
-
-# although t isn't used for anything, it must be initialized and passed
-t = np.linspace(0,10,101)
-
-# create a linspace for x to move from -.5 to .5 (and same for y)
-deltaX = np.linspace(-.5,.5,10000)
-deltaY = np.linspace(-.5,.5,10000)
-deltaZ = np.linspace(.8,1.2,1000)
-
-# create the vector r (which represents the floating atom) w/ constant y & z
-r_x = [deltaX, 0, 1]
-
-r_z = [0, 0, deltaZ]
-# this time create r with y changing, x & z constant
-r_y = [1, deltaY, .8]
-
-# initialize an array to hold the values of the VDW force at each x
-vdwForces = []
-
-# now we loop through the changing x values, calling vdwForce at each step
-# in order to find the total VDW force exerted on the floating atom at
-# each x value
-for i in range(len(deltaZ)):
-    vdwForces.append(vdwForce([r_z[0],r_z[1],r_z[2][i]],t)[2])
-
-# now we plot deltaX on the x-axis and vdwForces on the y-axis
-plt.figure()
-
-plt.plot(deltaZ,vdwForces,'k')
-
-plt.plot([.8, 1.2],[0, 0])
-
-# label the axes
-plt.xlabel('z')
-plt.ylabel('Total VDW Force')
-
-# set axes limits
-#plt.xlim([-1,1])
-#plt.ylim([-1,1])
-
-#plt.savefig('deltaY_vs_VDWforce_5_8.png')
-plt.show()
